86ed6947772460beee1dc541f8054baecdc91d5f
ktamoguis <ktamoguis@gmail.com>
2018-10-18 16:42:28 +0800

    change arrow function to standard function to reflect this

--- app/assets/javascripts/leadsmgt.js
+++ app/assets/javascripts/leadsmgt.js
@@ -13,7 +13,13 @@ function attachListeners() {
   //$('#clear').on('click', ()=> clearGame());
   $(".js-next").on("click", (e)=> nextLead(e));
   $(".leads_index").on("click", (e)=> leadsIndex(e));
-  $(document).on("click", ".show_link", (e)=>showLead(e));
+  //$(document).on("click", ".show_link", (e)=>showLead(e));
+  $(document).on("click", ".show_link", function(e){
+    e.preventDefault();
+    //debugger;
+    var lead = $(this).attr('data-id');
+    debugger;
+  });
   //$(".show_link").on("click", (e)=> showLead(e));
   //$(".js-index").on("click", (e)=> leadsIndex(e));
 }
@@ -23,6 +29,7 @@ function leadsIndex (e) {
   e.preventDefault();
   history.pushState(null,null,"leads");
   $.get("/leads.json", function(data) {
+    //debugger;
     $(".js_leads_index").html('');
     $("#lead_table").html('');
     var leadslist = "";
@@ -62,11 +69,23 @@ function Lead(lead){
 }
 
 function showLead (e) {
-  debugger;
+  //debugger;
   e.preventDefault();
+  //debugger;
+  var lead = $(this).attr('data-id');
   debugger;
-  let leadid = $(".show_link").attr("data-id")
+  var leadid = parseInt($(".show_link").attr("data-id"));
   debugger;
+  $.get("/leads/" + leadid + ".json", function(data) {
+    //debugger;
+    var lead = data;
+    console.log(data);
+    debugger;
+    //var leadslist = "";
+    //var leads = data;
+    //leadslist = "Lead Name" + ' ' + "Lead Status" + ' ' + "Booked Loans" + '</br>'
+  });
+  //debugger;
 
 }
 
